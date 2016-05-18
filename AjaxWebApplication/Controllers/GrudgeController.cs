610ca47fcd9cc77d79ed0d3b891a21882d63dcfd
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2016-05-18 11:05:45 +0200

    Grudge

---
+++ AjaxWebApplication/Controllers/GrudgeController.cs
@@ -0,0 +1,36 @@
+﻿using AjaxWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxWebApplication.Controllers
+{
+    public class GrudgeController : Controller
+    {
+        // GET: Grudge
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult _GrudgesForRabit(int id)
+        {
+            List<Grudge> grudges = ((Rabit)Session["Rabit"]).Grudges;
+            return PartialView(grudges);
+        }
+        public PartialViewResult _Create(int id)
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult _Create(Grudge model)
+        {
+            //Hämta från db dvs. Session
+            ((Rabit)Session["Rabit"]).Grudges.Add(model);
+            return RedirectToAction(actionName: "_GrudgesForRabit",
+                routeValues: new { id=9});
+        }
+
+    }
+}
