368ee61dfda8a21d99a38e4732220847491f8fb0
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2016-05-18 10:29:47 +0200

    A rabbit with na hat

---
+++ AjaxWebApplication/Controllers/RabitController.cs
@@ -0,0 +1,36 @@
+﻿using AjaxWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxWebApplication.Controllers
+{
+    public class RabitController : Controller
+    {
+        // GET: Rabit
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            Rabit rabit = new Rabit()
+            {
+                ID = 666,
+                BirthDate = new DateTime(year: 2006, month: 06, day: 06),
+                AngerLevel = 6,
+                Name = "The Devil",
+                Grudges = new List<Grudge>()
+                {
+                    new Grudge() { ID= 7, TargetName="Innocent rabits", Reason="ADHD" },
+                    new Grudge() { ID= 8, TargetName="People", Reason="Snitch" },
+                }              
+            };
+            //spara till DB... osv... vi gör fultrick... Session....
+            Session["Rabit"] = rabit;
+            return View(rabit);
+        }
+    }
+}
---
+++ AjaxWebApplication/Models/Rabit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjaxWebApplication.Models
+{
+    public class Rabit
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int AngerLevel { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List<Grudge> Grudges{ get; set; }
+
+    }
+}
